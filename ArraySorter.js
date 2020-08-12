2a9e60cdcc0626f224af603650dd00148838bd71
Danila Stryzhonok <36641744+ark23CIS@users.noreply.github.com>
2020-08-13 00:32:53 +0300

    bubbleSort commit - lower element to the left

--- ArraySorter.js
+++ ArraySorter.js
@@ -1,5 +1,26 @@
 class ArraySorter {
   constructor() {}
+
+  // BubbleSort - lower element goes to the left / higher element goes to the right
+  bubbleSort(arr) {
+    let n = arr.length;
+    for (let i = 0; i < n; i++) {
+      for (let j = n - 1; j >= i + 1; j--) {
+        if (arr[j] < arr[j - 1]) {
+          let a = arr[j],
+            b = arr[j - 1];
+          arr[j] = b;
+          arr[j - 1] = a;
+        }
+      }
+    }
+    return arr;
+  }
+  selectionSort(arr) {}
+  insertionSort(arr) {}
+  mergeSort(arr) {}
+  quickSort(arr) {}
+  shellSort(arr) {}
 }
 
 module.exports = ArraySorter;
