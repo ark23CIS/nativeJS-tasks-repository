369ef0bc60912028fc71cafb24df5a509f889710
Danila Stryzhonok <36641744+ark23CIS@users.noreply.github.com>
2020-08-13 11:17:29 +0300

    selectionSort commit

--- ArraySorter.js
+++ ArraySorter.js
@@ -1,9 +1,9 @@
 class ArraySorter {
   constructor() {}
-
   // BubbleSort - lower element goes to the left / higher element goes to the right
   bubbleSort(arr) {
-    let n = arr.length;
+    if (!arr.length) return [];
+    const n = arr.length;
     for (let i = 0; i < n; i++) {
       for (let j = n - 1; j >= i + 1; j--) {
         if (arr[j] < arr[j - 1]) [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];
@@ -11,8 +11,22 @@ class ArraySorter {
     }
     return arr;
   }
-  selectionSort(arr) {}
+  // selection - lower element goes to the start of the output array
+  selectionSort(arr) {
+    if (!arr.length) return [];
+    const n = arr.length;
+    for (let i = 0; i < n; i++) {
+      let minIndex = i;
+      for (let j = i + 1; j < n; j++) {
+        if (arr[minIndex] > arr[j]) minIndex = j;
+      }
+      if (minIndex !== i) [arr[minIndex], arr[i]] = [arr[i], arr[minIndex]];
+    }
+    return arr;
+  }
+  // insertion - current element inserts to sorted array
   insertionSort(arr) {
+    if (!arr.length) return [];
     for (let i = 1; i < arr.length; i++) {
       let key = arr[i];
       for (var j = i; j > 0 && arr[j - 1] > key; j--) {
